c5670e5806307c8791203c69ffe006cf4d221e24
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2018-05-21 21:29:31 +0300

    Remove export

--- src/app/common/utils/scroll-to-top.jsx
+++ src/app/common/utils/scroll-to-top.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
-export class ScrollToTop extends Component {
+class ScrollToTop extends Component {
 	componentDidUpdate(prevProps) {
 		if (this.props.location !== prevProps.location) {
 			window.scrollTo(0, 0);
